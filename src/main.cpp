8b6c6d3d599745e1d449648361f741f512def2fe
drfailer <drfailer.42@gmail.com>
2025-04-02 15:20:56 -0400

    fix: using wrong indexes in init

--- src/main.cpp
+++ src/main.cpp
@@ -35,17 +35,17 @@ void matmul(ftype *A, ftype *B, size_t n, size_t m, size_t k, ftype *C) {
   cudaFree(C_gpu);
 }
 
-void init(ftype *A, ftype *B, ftype *C, size_t m, size_t n, size_t k) {
+void init(ftype *A, ftype *B, ftype *C, int64_t m, int64_t n, int64_t k) {
   // init A
-  for (size_t i = 0; i < m; ++i) {
-    for (size_t j = 0; j < n; ++j) {
+  for (int64_t i = 0; i < m; ++i) {
+    for (int64_t j = 0; j < n; ++j) {
       A[i * n + j] = i + j;
     }
   }
 
   // init B
-  for (size_t i = 0; i < m; ++i) {
-    for (size_t j = 0; j < n; ++j) {
+  for (int64_t i = 0; i < n; ++i) {
+    for (int64_t j = 0; j < k; ++j) {
       B[i * k + j] = i * j;
     }
   }
