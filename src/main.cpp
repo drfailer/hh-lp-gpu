4b70421fdb68d8b3d64ed3d414bd09580fd1bc1e
drfailer <drfailer.42@gmail.com>
2025-04-03 15:32:02 -0400

    working matrix multiplication and hadamard product

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,74 @@
 using ftype = float;
 
 /*
+ * C[m, n] = A[m, n] 0 B[m, n]
+ */
+void hadamard(ftype *A_host, ftype *B_host, int64_t m, int64_t n, ftype *C_host) {
+  namespace fe = cudnn_frontend;
+
+  ftype *A_gpu = nullptr, *B_gpu = nullptr;
+  ftype *C_gpu = nullptr;
+
+  cudaMalloc((void **)(&A_gpu), m * n * sizeof(*A_gpu));
+  cudaMalloc((void **)(&B_gpu), m * n * sizeof(*B_gpu));
+  cudaMalloc((void **)(&C_gpu), m * n * sizeof(*C_gpu));
+
+  assert(cudaSuccess == cudaMemcpy(A_gpu, A_host, m * n * sizeof(*A_gpu), cudaMemcpyHostToDevice));
+  assert(cudaSuccess == cudaMemcpy(B_gpu, B_host, m * n * sizeof(*B_gpu), cudaMemcpyHostToDevice));
+  cudaDeviceSynchronize();
+
+  fe::graph::Graph graph;
+
+  auto A = graph.tensor(fe::graph::Tensor_attributes()
+                            .set_name("A")
+                            .set_dim({1, m, n})
+                            .set_stride({m * n, n, 1})
+                            .set_data_type(fe::DataType_t::FLOAT));
+  auto B = graph.tensor(fe::graph::Tensor_attributes()
+                            .set_name("B")
+                            .set_dim({1, m, n})
+                            .set_stride({m * n, n, 1})
+                            .set_data_type(fe::DataType_t::FLOAT));
+
+  auto C = graph.pointwise(A, B,
+                           fe::graph::Pointwise_attributes()
+                               .set_name("hadamard")
+                               .set_mode(fe::PointwiseMode_t::MUL)
+                               .set_compute_data_type(fe::DataType_t::FLOAT));
+  C->set_output(true).set_data_type(fe::DataType_t::FLOAT);
+
+  CUDNN_CHECK(graph.validate());
+
+  std::unordered_map<std::shared_ptr<fe::graph::Tensor_attributes>, void *>
+      memory_map = {{A, A_gpu}, {B, B_gpu}, {C, C_gpu}};
+
+  cudnnHandle_t handle;
+  cudnnCreate(&handle);
+
+  CUDNN_CHECK(graph.build_operation_graph(handle));
+  CUDNN_CHECK(graph.create_execution_plans({fe::HeurMode_t::A}));
+  CUDNN_CHECK(graph.check_support(handle));
+  CUDNN_CHECK(
+      graph.build_plans(handle, fe::BuildPlanPolicy_t::HEURISTICS_CHOICE));
+
+  int64_t workspace_size;
+  CUDNN_CHECK(graph.get_workspace_size(workspace_size));
+  float *workspace = nullptr;
+  cudaMalloc((void **)(&workspace), workspace_size * sizeof(*workspace));
+
+  CUDNN_CHECK(graph.execute(handle, memory_map, workspace));
+
+  assert(cudaSuccess == cudaMemcpy(C_host, C_gpu, m * n * sizeof(*C_gpu), cudaMemcpyDeviceToHost));
+  cudaDeviceSynchronize();
+
+  cudaFree(workspace);
+  cudnnDestroy(handle);
+  cudaFree(A_gpu);
+  cudaFree(B_gpu);
+  cudaFree(C_gpu);
+}
+
+/*
  * C[m, k] = A[m, n] * B[n, k]
  */
 void matmul(ftype *A_host, ftype *B_host, int64_t m, int64_t n, int64_t k,
@@ -38,24 +106,19 @@ void matmul(ftype *A_host, ftype *B_host, int64_t m, int64_t n, int64_t k,
 
   auto A = graph.tensor(fe::graph::Tensor_attributes()
                             .set_name("A")
-                            .set_dim({m, n, 1})
-                            .set_stride({n, 1, 1})
+                            .set_dim({1, m, n})
+                            .set_stride({m * n , n, 1})
                             .set_data_type(fe::DataType_t::FLOAT));
   auto B = graph.tensor(fe::graph::Tensor_attributes()
                             .set_name("B")
-                            .set_dim({n, k, 1})
-                            .set_stride({k, 1, 1})
+                            .set_dim({1, n, k})
+                            .set_stride({n * k, k, 1})
                             .set_data_type(fe::DataType_t::FLOAT));
 
-  /* auto C = graph.matmul( */
-  /*     A, B, */
-  /*     fe::graph::Matmul_attributes().set_name("matmul").set_compute_data_type( */
-  /*         fe::DataType_t::FLOAT)); */
-  auto C = graph.pointwise(A, B,
-                           fe::graph::Pointwise_attributes()
-                               .set_name("hadamard")
-                               .set_mode(fe::PointwiseMode_t::MUL)
-                               .set_compute_data_type(fe::DataType_t::FLOAT));
+  auto C = graph.matmul(
+      A, B,
+      fe::graph::Matmul_attributes().set_name("matmul").set_compute_data_type(
+          fe::DataType_t::FLOAT));
   C->set_output(true).set_data_type(fe::DataType_t::FLOAT);
 
   CUDNN_CHECK(graph.validate());
@@ -122,9 +185,9 @@ void display_matrix(std::string const &name, ftype *matrix, int64_t rows,
 
 int main(int, char **) {
   std::cout << "testing cudnn and cublas" << std::endl;
-  constexpr int64_t m = 10;
-  constexpr int64_t n = 10;
-  constexpr int64_t k = 10;
+  constexpr int64_t m = 3;
+  constexpr int64_t n = 3;
+  constexpr int64_t k = 3;
   ftype *A = new ftype[m * n];
   ftype *B = new ftype[n * k];
   ftype *C = new ftype[m * k];
@@ -134,8 +197,10 @@ int main(int, char **) {
   display_matrix("B", B, n, k);
   display_matrix("C", C, m, k);
 
-  matmul(A, B, m, n, k, C);
+  if (m == n && n == k) hadamard(A, B, m, n, C);
+  display_matrix("C", C, m, k);
 
+  matmul(A, B, m, n, k, C);
   display_matrix("C", C, m, k);
 
   delete[] A;
