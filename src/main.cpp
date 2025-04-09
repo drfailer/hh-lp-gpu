57a61a6677a8819edd94b35153a0e98b64f8acd1
drfailer <drfailer.42@gmail.com>
2025-04-09 11:19:03 -0400

    move cudnn test code in a separated function

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
+#include "cudnn_operations.hpp"
 #include <cstdint>
 #include <cstring>
 #include <iostream>
-#include "cudnn_operations.hpp"
 
 // design:
 //
@@ -56,58 +56,62 @@
 // - BackwardPassState
 
 void init(ftype *A, ftype *B, ftype *C, int64_t m, int64_t n, int64_t k) {
-  // init A
-  for (int64_t i = 0; i < m; ++i) {
-    for (int64_t j = 0; j < n; ++j) {
-      A[i * n + j] = i + j;
+    // init A
+    for (int64_t i = 0; i < m; ++i) {
+        for (int64_t j = 0; j < n; ++j) {
+            A[i * n + j] = i + j;
+        }
     }
-  }
 
-  // init B
-  for (int64_t i = 0; i < n; ++i) {
-    for (int64_t j = 0; j < k; ++j) {
-      B[i * k + j] = i * j;
+    // init B
+    for (int64_t i = 0; i < n; ++i) {
+        for (int64_t j = 0; j < k; ++j) {
+            B[i * k + j] = i * j;
+        }
     }
-  }
 
-  // init C to zero
-  memset(C, 0, m * k * sizeof(*C));
+    // init C to zero
+    memset(C, 0, m * k * sizeof(*C));
 }
 
 void display_matrix(std::string const &name, ftype *matrix, int64_t rows,
                     int64_t cols) {
-  std::cout << name << " =" << std::endl;
-  for (int64_t i = 0; i < rows; ++i) {
-    std::cout << "\t";
-    for (int64_t j = 0; j < cols; ++j) {
-      std::cout << (float)matrix[i * cols + j] << " ";
+    std::cout << name << " =" << std::endl;
+    for (int64_t i = 0; i < rows; ++i) {
+        std::cout << "\t";
+        for (int64_t j = 0; j < cols; ++j) {
+            std::cout << (float)matrix[i * cols + j] << " ";
+        }
+        std::cout << std::endl;
     }
-    std::cout << std::endl;
-  }
+}
+
+void test_cdnn_operations() {
+    constexpr int64_t m = 3;
+    constexpr int64_t n = 3;
+    constexpr int64_t k = 3;
+    ftype *A = new ftype[m * n];
+    ftype *B = new ftype[n * k];
+    ftype *C = new ftype[m * k];
+
+    init(A, B, C, m, n, k);
+    display_matrix("A", A, m, n);
+    display_matrix("B", B, n, k);
+    display_matrix("C", C, m, k);
+
+    if (m == n && n == k)
+        hadamard(A, B, m, n, C);
+    display_matrix("C", C, m, k);
+
+    matmul(A, B, m, n, k, C);
+    display_matrix("C", C, m, k);
+
+    delete[] A;
+    delete[] B;
+    delete[] C;
 }
 
 int main(int, char **) {
-  std::cout << "testing cudnn and cublas" << std::endl;
-  constexpr int64_t m = 3;
-  constexpr int64_t n = 3;
-  constexpr int64_t k = 3;
-  ftype *A = new ftype[m * n];
-  ftype *B = new ftype[n * k];
-  ftype *C = new ftype[m * k];
-
-  init(A, B, C, m, n, k);
-  display_matrix("A", A, m, n);
-  display_matrix("B", B, n, k);
-  display_matrix("C", C, m, k);
-
-  if (m == n && n == k) hadamard(A, B, m, n, C);
-  display_matrix("C", C, m, k);
-
-  matmul(A, B, m, n, k, C);
-  display_matrix("C", C, m, k);
-
-  delete[] A;
-  delete[] B;
-  delete[] C;
-  return 0;
+    test_cdnn_operations();
+    return 0;
 }
