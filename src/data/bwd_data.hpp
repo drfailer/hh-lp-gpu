1da07901b6e2b813d2c9f6c44727e5bd9da33e51
drfailer <drfailer.42@gmail.com>
2025-04-18 08:27:01 -0400

    remove unused includes

--- src/data/bwd_data.hpp
+++ src/data/bwd_data.hpp
@@ -1,7 +1,6 @@
 #ifndef DATA_BWD_DATA_H
 #define DATA_BWD_DATA_H
 #include "layer_state.hpp"
-#include <vector>
 
 template <typename T> struct BwdData {
     NetworkState<T> &states;
--- src/data/fwd_data.hpp
+++ src/data/fwd_data.hpp
@@ -1,7 +1,6 @@
 #ifndef DATA_FWD_DATA_H
 #define DATA_FWD_DATA_H
 #include "layer_state.hpp"
-#include <vector>
 
 template <typename T> struct FwdData {
     NetworkState<T> &states;
