f1cbccd0afd1e6cbc8e5f8468a8fa2b9168c99cf
drfailer <drfailer.42@gmail.com>
2025-04-20 11:12:27 -0400

    init data

--- src/data/init_data.hpp
+++ src/data/init_data.hpp
@@ -2,7 +2,12 @@
 #define DATA_INIT_DATA_H
 #include "layer_state.hpp"
 
-template <typename T> struct InitData {
+enum class InitStatus {
+    Init,
+    Done,
+};
+
+template <typename T, InitStatus status = InitStatus::Init> struct InitData {
     NetworkState<T> &states;
 };
 
