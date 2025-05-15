aa3f90642d8c8997fcf37ec0af8971f12b64baaa
drfailer <drfailer.42@gmail.com>
2025-05-15 13:12:54 -0400

    batch count in sigmoid layer

--- src/model/layer/sigmoid_activation_layer.hpp
+++ src/model/layer/sigmoid_activation_layer.hpp
@@ -2,10 +2,10 @@
 #define MODEL_LAYER_SIGMOID_ACTIVATION_LAYER_H
 #include "../../types.hpp"
 #include "layer.hpp"
-#include <cudnn_ops.h>
-#include <log.h/log.h>
 #include <cudnn.h>
 #include <cudnn_graph.h>
+#include <cudnn_ops.h>
+#include <log.h/log.h>
 
 struct SigmoidActivationLayer : Layer<ftype> {
     SigmoidActivationLayer(cudnnHandle_t cudnn_handle, int64_t size)
@@ -18,19 +18,23 @@ struct SigmoidActivationLayer : Layer<ftype> {
         // input tensor
         cudnnCreateTensorDescriptor(&fwd_.input_tensor);
         cudnnSetTensor4dDescriptor(fwd_.input_tensor, CUDNN_TENSOR_NCHW,
-                                   CUDNN_DATA_FLOAT, 1, 1, dims.inputs, 1);
+                                   CUDNN_DATA_FLOAT, dims.batch_count, 1,
+                                   dims.inputs, 1);
         // output tensor
         cudnnCreateTensorDescriptor(&fwd_.output_tensor);
         cudnnSetTensor4dDescriptor(fwd_.output_tensor, CUDNN_TENSOR_NCHW,
-                                   CUDNN_DATA_FLOAT, 1, 1, dims.inputs, 1);
+                                   CUDNN_DATA_FLOAT, dims.batch_count, 1,
+                                   dims.inputs, 1);
         // input error tensor
         cudnnCreateTensorDescriptor(&bwd_.err_tensor);
         cudnnSetTensor4dDescriptor(bwd_.err_tensor, CUDNN_TENSOR_NCHW,
-                                   CUDNN_DATA_FLOAT, 1, 1, dims.inputs, 1);
+                                   CUDNN_DATA_FLOAT, dims.batch_count, 1,
+                                   dims.inputs, 1);
         // output error tensor
         cudnnCreateTensorDescriptor(&bwd_.output_tensor);
         cudnnSetTensor4dDescriptor(bwd_.output_tensor, CUDNN_TENSOR_NCHW,
-                                   CUDNN_DATA_FLOAT, 1, 1, dims.inputs, 1);
+                                   CUDNN_DATA_FLOAT, dims.batch_count, 1,
+                                   dims.inputs, 1);
     }
 
     ~SigmoidActivationLayer() {
@@ -43,8 +47,8 @@ struct SigmoidActivationLayer : Layer<ftype> {
 
     /*
      * Allocates memory for a layer state (output memory for the fwd pass, bwd
-     * pass, parameters and gradiants). Since this layer is an activation one,
-     * there no need to allocate parameters and gradiants.
+     * pass, parameters and gradients). Since this layer is an activation one,
+     * there no need to allocate parameters and gradients.
      */
     void init(LayerState<ftype> &state) override {
         state = create_layer_state<ftype>(this->dims, false, false);
