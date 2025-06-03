def2925640f79cb2cd6967f3592817284d9a448c
drfailer <drfailer.42@gmail.com>
2025-06-03 09:16:58 -0400

    helper debug macro for printing tensor descriptors

--- src/model/data/tensor.hpp
+++ src/model/data/tensor.hpp
@@ -4,7 +4,7 @@
 #include "../../types.hpp"
 #include "dims.hpp"
 #include <array>
-#include <cstdint>
+#include <cstdio>
 #include <cudnn_ops.h>
 
 using vec_t = std::array<int, 4>;
@@ -13,11 +13,15 @@ template <typename T> class Tensor {
   public:
     Tensor(vec_t const &dims = {0}, vec_t const &strides = {0})
         : dims_(dims), strides_(strides) {
-        CUDA_CHECK(alloc_gpu(&data_, dims[0] * dims[1] * dims[2] * dims[3]));
-        cudnnCreateTensorDescriptor(&descriptor_);
-        cudnnSetTensor4dDescriptorEx(descriptor_, CUDNN_DATA_TYPE, dims[0],
-                                     dims[1], dims[2], dims[3], strides[0],
-                                     strides[1], strides[2], strides[3]);
+        data_size_ = dims[0] * dims[1] * dims[2] * dims[3];
+        CUDNN_CHECK(cudnnCreateTensorDescriptor(&descriptor_));
+
+        if (data_size_ == 0)
+            return;
+        CUDA_CHECK(alloc_gpu(&data_, data_size_));
+        CUDNN_CHECK(cudnnSetTensor4dDescriptorEx(
+            descriptor_, CUDNN_DATA_TYPE, dims[0], dims[1], dims[2], dims[3],
+            strides[0], strides[1], strides[2], strides[3]));
     }
 
     Tensor(Tensor const &) = delete;
@@ -28,6 +32,7 @@ template <typename T> class Tensor {
         std::swap(this->dims_, other.dims_);
         std::swap(this->strides_, other.strides_);
         std::swap(this->descriptor_, other.descriptor_);
+        std::swap(this->data_size_, other.data_size_);
     }
 
     ~Tensor() {
@@ -40,6 +45,7 @@ template <typename T> class Tensor {
     vec_t const &dims() const { return dims_; }
     vec_t const &strides() const { return strides_; }
     cudnnTensorDescriptor_t descriptor() const { return descriptor_; }
+    size_t data_size() const { return data_size_; }
 
     /*
      * Note: we assume that this function will always be called with a complete
@@ -52,10 +58,11 @@ template <typename T> class Tensor {
         cudaFree(data_);
         dims_ = dims;
         strides_ = strides;
-        CUDA_CHECK(alloc_gpu(&data_, dims[0] * dims[1] * dims[2] * dims[3]));
-        cudnnSetTensor4dDescriptorEx(descriptor_, CUDNN_DATA_TYPE, dims[0],
-                                     dims[1], dims[2], dims[3], strides[0],
-                                     strides[1], strides[2], strides[3]);
+        data_size_ = dims[0] * dims[1] * dims[2] * dims[3];
+        CUDA_CHECK(alloc_gpu(&data_, data_size_));
+        CUDNN_CHECK(cudnnSetTensor4dDescriptorEx(
+            descriptor_, CUDNN_DATA_TYPE, dims[0], dims[1], dims[2], dims[3],
+            strides[0], strides[1], strides[2], strides[3]));
     }
 
     void reshape(vec_t const &dims) {
@@ -81,6 +88,7 @@ template <typename T> class Tensor {
     vec_t dims_ = {};
     vec_t strides_ = {};
     cudnnTensorDescriptor_t descriptor_ = nullptr;
+    size_t data_size_;
 };
 
 template <typename T>
@@ -95,9 +103,18 @@ template <typename T> Tensor<T> *create_tensor(vec_t const &dims) {
 
 template <typename T>
 Tensor<T> *create_tensor_from_dims(tensor_dims_t const &dims) {
-    return create_tensor<T>(
-        {dims.n, dims.c, dims.h, dims.w},
-        {dims.c * dims.h * dims.w, dims.h * dims.w, dims.w, 1});
+    return create_tensor<T>({dims.n, dims.c, dims.h, dims.w});
 }
+
+#define print_tensor_descriptor(desc)                                          \
+    {                                                                          \
+        int n, c, h, w;                                                        \
+        int ns, cs, hs, ws;                                                    \
+        cudnnDataType_t data_type;                                             \
+        cudnnGetTensor4dDescriptor(desc, &data_type, &n, &c, &h, &w, &ns, &cs, \
+                                   &hs, &ws);                                  \
+        printf(#desc ": [%d, %d, %d, %d]%d : (%d, %d, %d, %d)\n", n, c, h, w,    \
+               data_type, ns, cs, hs, ws);                                     \
+    }
 
 #endif
