3401dd3afbb96f6c7efbc0a862b6d4963e6c3c42
drfailer <drfailer.42@gmail.com>
2025-06-20 16:38:48 -0400

    optimized _hhlpLinearForward kernel

--- src/kernels/linear_layer_kernel.cu
+++ src/kernels/linear_layer_kernel.cu
@@ -6,44 +6,68 @@
 /*                                cuda kernels                                */
 /******************************************************************************/
 
+__device__ __half operator+=(__half volatile &lhs, __half volatile const &rhs) {
+    return (__half &)lhs += (const __half &)rhs;
+}
+
+template <unsigned int BLOCK_SIZE, typename DataType>
+__device__ void warp_reduce(volatile DataType *shared_data, unsigned int tid) {
+    // clang-format off
+    if (BLOCK_SIZE >= 64) { if (tid < 32) shared_data[tid] += shared_data[tid + 32]; }
+    if (BLOCK_SIZE >= 32) { if (tid < 16) shared_data[tid] += shared_data[tid + 16]; }
+    if (BLOCK_SIZE >= 16) { if (tid < 8)  shared_data[tid] += shared_data[tid + 8]; }
+    if (BLOCK_SIZE >= 8) { if (tid < 4)  shared_data[tid] += shared_data[tid + 4]; }
+    if (BLOCK_SIZE >= 4) { if (tid < 2)  shared_data[tid] += shared_data[tid + 2]; }
+    if (BLOCK_SIZE >= 2) { if (tid < 1)  shared_data[tid] += shared_data[tid + 1]; }
+    // clang-format on
+}
+
 /*
  * output = weights * input + biases
- * TODO: use the x axis to compute over the batch
- * 1 block per row
- * blockIdx.x -> row
+ *
+ * blockIdx.x -> output row
  * threadIdx.y -> batch
  * threadIdx.x -> partial sum over the row
- * TODO: https://developer.download.nvidia.com/assets/cuda/files/reduction.pdf
- *       (slide 35)
+ *
+ * source: https://developer.download.nvidia.com/assets/cuda/files/reduction.pdf
  */
-template <int BLOCK_SIZE, typename DataType>
+template <unsigned int REDUCE_BLOCK_SIZE, unsigned int BATCH_BLOCK_SIZE,
+          typename DataType>
 __global__ void
 _hhlpLinearForward(DataType const *weights, DataType const *biases,
                    DataType const *inputs, DataType *outputs, int nb_inputs,
                    int nb_outputs, int batch_size) {
-    int batch_idx = blockIdx.y * BLOCK_SIZE + threadIdx.y;
+    int batch_idx = blockIdx.y * BATCH_BLOCK_SIZE + threadIdx.y;
+    unsigned int tid = threadIdx.x;
 
     if (batch_idx >= batch_size)
         return;
 
-    __shared__ DataType result[BLOCK_SIZE][BLOCK_SIZE];
+    __shared__ DataType results[BATCH_BLOCK_SIZE * REDUCE_BLOCK_SIZE];
 
-    result[threadIdx.y][threadIdx.x] = 0;
-    for (int idx = threadIdx.x; idx < nb_inputs; idx += BLOCK_SIZE) {
-        DataType w = weights[blockIdx.x * nb_inputs + idx];
-        DataType i = inputs[batch_idx * nb_inputs + idx];
-        result[threadIdx.y][threadIdx.x] += w * i;
+    DataType *result = &results[threadIdx.y * REDUCE_BLOCK_SIZE];
+    DataType const *weights_row = &weights[blockIdx.x * nb_inputs];
+    DataType const *input = &inputs[batch_idx * nb_inputs];
+
+    result[tid] = 0;
+    for (int idx = tid; idx < nb_inputs; idx += REDUCE_BLOCK_SIZE) {
+        DataType w = weights_row[idx];
+        DataType i = input[idx];
+        result[tid] += w * i;
     }
     __syncthreads();
 
-    for (int idx = BLOCK_SIZE / 2; idx > 0; idx /= 2) {
-        if (threadIdx.x < idx) {
-            result[threadIdx.y][threadIdx.x] +=
-                result[threadIdx.y][threadIdx.x + idx];
-        }
+    // clang-format off
+    if (REDUCE_BLOCK_SIZE >= 512) { if (tid < 256) { result[tid] += result[tid + 256]; } __syncthreads(); }
+    if (REDUCE_BLOCK_SIZE >= 256) { if (tid < 128) { result[tid] += result[tid + 128]; } __syncthreads(); }
+    if (REDUCE_BLOCK_SIZE >= 128) { if (tid < 64) { result[tid] += result[tid + 64]; } __syncthreads(); }
+    warp_reduce<REDUCE_BLOCK_SIZE, DataType>(result, tid);
+    // clang-format on
+
+    if (tid == 0) {
+        outputs[batch_idx * nb_outputs + blockIdx.x] =
+            result[0] + biases[blockIdx.x];
     }
-    outputs[batch_idx * nb_outputs + blockIdx.x] =
-        result[threadIdx.y][0] + biases[blockIdx.x];
 }
 
 /*
@@ -116,30 +140,6 @@ _hhlpLinearBackwardData(DataType const *output_gradients,
 /*                             external functions                             */
 /******************************************************************************/
 
-#define EXTERNAL_FUNCTION_IMPL(KERNEL, ...)                                    \
-    cudnnStatus_t KERNEL(__VA_ARGS__) {                                        \
-        cudaStream_t stream;                                                   \
-        dim3 threads(1, std::min(nb_outputs, 32));                             \
-        dim3 grid(1, std::max(1, nb_outputs / 32));                            \
-                                                                               \
-        cudnnGetStream(cudnn_handle, &stream);                                 \
-        switch (data_type) {                                                   \
-        case CUDNN_DATA_FLOAT:                                                 \
-            KERNEL<32><<<grid, threads, 0, stream>>>(KERNEL_PARAMS(float));    \
-            break;                                                             \
-        case CUDNN_DATA_DOUBLE:                                                \
-            KERNEL<32><<<grid, threads, 0, stream>>>(KERNEL_PARAMS(double));   \
-            break;                                                             \
-        case CUDNN_DATA_HALF:                                                  \
-            KERNEL<32><<<grid, threads, 0, stream>>>(KERNEL_PARAMS(int16_t));  \
-            break;                                                             \
-        default:                                                               \
-            return cudnnStatus_t::CUDNN_STATUS_NOT_SUPPORTED_DATA_TYPE;        \
-            break;                                                             \
-        }                                                                      \
-        return cudnnStatus_t::CUDNN_STATUS_SUCCESS;                            \
-    }
-
 #define LAUNCH_KERNEL(target_type, kernel)                                     \
     {                                                                          \
         using type = target_type;                                              \
@@ -171,15 +171,19 @@ cudnnStatus_t hhlpLinearForward(cudnnHandle_t cudnn_handle, void const *weights,
     cudaStream_t stream;
     cudnnGetStream(cudnn_handle, &stream);
 
+    constexpr unsigned int batch_threads = 32;
+    constexpr unsigned int reduce_threads = 32;
+
     // TODO: compute the number of threads using the warp tile size
-    dim3 threads(16, 64, 1);
-    dim3 grid(nb_outputs, CEIL_DIV(batch_size, threads.z), 1);
+    dim3 threads(reduce_threads, batch_threads, 1);
+    dim3 grid(nb_outputs, CEIL_DIV(batch_size, threads.y), 1);
 
     SWITCH_CUDNN_TYPE(
         data_type,
-        (_hhlpLinearForward<64><<<grid, threads, 0, stream>>>(
-            (type const *)weights, (type const *)biases, (type const *)input,
-            (type *)output, nb_inputs, nb_outputs, batch_size)));
+        (_hhlpLinearForward<reduce_threads, batch_threads>
+         <<<grid, threads, 0, stream>>>(
+             (type const *)weights, (type const *)biases, (type const *)input,
+             (type *)output, nb_inputs, nb_outputs, batch_size)));
     return cudnnStatus_t::CUDNN_STATUS_SUCCESS;
 }
 
@@ -190,13 +194,20 @@ cudnnStatus_t hhlpLinearBackwardBias(cudnnHandle_t cudnn_handle,
     cudaStream_t stream;
     cudnnGetStream(cudnn_handle, &stream);
 
-    dim3 threads(1, 32);
-    dim3 grid(1, CEIL_DIV(nb_outputs, threads.y));
+    if (batch_size == 1) {
+        SWITCH_CUDNN_TYPE(data_type, cudaMemcpy(biases_gradient, error,
+                                                nb_outputs * sizeof(type),
+                                                cudaMemcpyDeviceToDevice));
+    } else {
+        dim3 threads(1, 32);
+        dim3 grid(1, CEIL_DIV(nb_outputs, threads.y));
 
-    SWITCH_CUDNN_TYPE(
-        data_type, (_hhlpLinearBackwardBias<32><<<grid, threads, 0, stream>>>(
-                       (type const *)error, (type *)biases_gradient, nb_outputs,
-                       batch_size)));
+        SWITCH_CUDNN_TYPE(
+            data_type,
+            (_hhlpLinearBackwardBias<32><<<grid, threads, 0, stream>>>(
+                (type const *)error, (type *)biases_gradient, nb_outputs,
+                batch_size)));
+    }
     return cudnnStatus_t::CUDNN_STATUS_SUCCESS;
 }
 
@@ -214,7 +225,7 @@ cudnnStatus_t hhlpLinearBackwardWeights(cudnnHandle_t cudnn_handle,
 
     SWITCH_CUDNN_TYPE(
         data_type,
-        (_hhlpLinearBackwardWeights<4><<<grid, threads, 0, stream>>>(
+        (_hhlpLinearBackwardWeights<32><<<grid, threads, 0, stream>>>(
             (type *)output_gradient, (type *)input, (type *)weights_gradient,
             nb_outputs, nb_inputs, batch_size)));
     return cudnnStatus_t::CUDNN_STATUS_SUCCESS;
