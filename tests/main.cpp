d2a82d9a5f36e6dcde0ddd61d116fc62b7f92770
drfailer <drfailer.42@gmail.com>
2025-06-27 11:01:28 -0400

    update tests

--- tests/main.cpp
+++ tests/main.cpp
@@ -15,25 +15,25 @@ int main(int, char **) {
 
     utest_start();
 
-    // urun_test(matvecmul_n);
-    // urun_test(matvecmul_t);
-    // urun_test(matvecmul_batch_n);
-    // urun_test(matmul_n_n);
-    // urun_test(matmul_t_n);
-    // urun_test(matmul_n_t);
-    // urun_test(matmul_t_t);
-    // urun_test(matmul_batch_n_n);
+    urun_test(matvecmul_n);
+    urun_test(matvecmul_t);
+    urun_test(matvecmul_batch_n);
+    urun_test(matmul_n_n);
+    urun_test(matmul_t_n);
+    urun_test(matmul_n_t);
+    urun_test(matmul_t_t);
+    urun_test(matmul_batch_n_n);
 
-    // urun_test(linear_layer_fwd);
-    // urun_test(linear_layer_bwd);
-    // urun_test(linear_layer_fwd_batched);
-    // urun_test(linear_layer_bwd_batched);
-    // urun_test(sigmoid_activation_fwd);
-    // urun_test(sigmoid_activation_bwd);
-    // urun_test(sgd_optimizer);
+    urun_test(linear_layer_fwd);
+    urun_test(linear_layer_bwd);
+    urun_test(linear_layer_fwd_batched);
+    urun_test(linear_layer_bwd_batched);
+    urun_test(sigmoid_activation_fwd);
+    urun_test(sigmoid_activation_bwd);
+    urun_test(sgd_optimizer);
 
-    // urun_test(inference);
-    // urun_test(training);
+    urun_test(inference);
+    urun_test(training);
 
     urun_test(mnist);
     urun_test(mnist_batched);
