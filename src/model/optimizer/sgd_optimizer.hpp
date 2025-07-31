bfe4e69a2e120ce13deb725a4953c126454d3155
drfailer <drfailer.42@gmail.com>
2025-07-31 15:57:18 -0400

    update optimizer

--- src/model/optimizer/sgd_optimizer.hpp
+++ src/model/optimizer/sgd_optimizer.hpp
@@ -13,27 +13,23 @@ struct SGDOptimizer : Optimizer<ftype> {
 
     SGDOptimizer(ftype learning_rate) : learning_rate(learning_rate) {}
 
-    void optimize(cuda_data_t cuda_data, LayerData<ftype> &state) override {
+    void optimize(cuda_data_t cuda, LayerData<ftype> &data) override {
         INFO_GRP("Optimizer", INFO_GRP_LAYER_TASK);
 
         // params = params - learning_rate * gradients
 
         ftype alpha = -learning_rate, beta = 1;
 
-        if (!state.w.empty()) {
-            CUDNN_CHECK(cudnnAddTensor(cuda_data.cudnn_handle, &alpha,
-                                       state.dw.desc(),
-                                       state.dw.data(), &beta,
-                                       state.w.desc(),
-                                       state.w.data()));
+        if (!data.w.empty() && data.w.size() > 0) {
+            CUDNN_CHECK(cudnnAddTensor(cuda.cudnn_handle, &alpha,
+                                       data.dw.desc(), data.dw.data(), &beta,
+                                       data.w.desc(), data.w.data()));
         }
 
-        if (!state.b.empty()) {
-            CUDNN_CHECK(cudnnAddTensor(cuda_data.cudnn_handle, &alpha,
-                                       state.db.desc(),
-                                       state.db.data(), &beta,
-                                       state.b.desc(),
-                                       state.b.data()));
+        if (!data.b.empty() && data.b.size() > 0) {
+            CUDNN_CHECK(cudnnAddTensor(cuda.cudnn_handle, &alpha,
+                                       data.db.desc(), data.db.data(), &beta,
+                                       data.b.desc(), data.b.data()));
         }
     }
 
