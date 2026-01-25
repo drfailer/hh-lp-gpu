119ec9a02a6c3a7d221900f64879098043561e78
drfailer <drfailer.42@gmail.com>
2026-01-25 13:37:04 -0500

    communicator header

---
+++ src/tools/communicator.hpp
@@ -0,0 +1,10 @@
+#ifndef TOOLS_COMMUNICATOR
+#define TOOLS_COMMUNICATOR
+
+#include <hedgehog/communicator/communicator_task.hpp>
+#include <hedgehog/communicator/service/mpi_service.hpp>
+
+using CommService = hh::comm::CommService;
+using MPIService = hh::comm::MPIService;
+
+#endif
